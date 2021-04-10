0e99a36a89e975b84488b85b850bbf4807c622d0
unknown <andriamandayandymihaja@gmail.com>
2021-04-10 18:08:24 +0300

    Projet de depart avec modif serveur

---
+++ api/README.md
@@ -0,0 +1,4 @@
+# MBDS_Madagascar2021_backend
+Backend Et Frontend NodeJS Mongoose pour le cours angular MBDS Madagascar 2021
+Le frontend (dont le resultat de la build est dans le dossier frontdist ) est servi par le server.js dans ce dossier ( pour faciliter le déployement)
+Il suffit de lancer `node server.js` pour lancer le Frontend(dernière build) et le Backend. 
---
+++ api/server.js
@@ -0,0 +1,69 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+const path = require("path");
+
+let assignment = require('./routes/assignments');
+
+let mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+//mongoose.set('debug', true);
+
+const uri = "mongodb+srv://andy:master2021root@cluster0.4nfqp.mongodb.net/assignmentdb?retryWrites=true&w=majority";
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify:false
+};
+
+mongoose.connect(uri, options)
+  .then(() => {
+    console.log("Connecté à la base MongoDB assignments dans le cloud !");
+    console.log("at URI = " + uri);
+    console.log("vérifiez with http://localhost:8010/api/assignments que cela fonctionne")
+    },
+    err => {
+      console.log('Erreur de connexion: ', err);
+    });
+
+// Pour accepter les connexions cross-domain (CORS)
+app.use((req, res, next) =>  {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+// Pour les formulaires
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+let port = process.env.PORT || 8010;
+
+// les routes
+const prefix = '/api';
+
+app.route(prefix + '/assignments')
+  .get(assignment.getAssignments)
+  .post(assignment.postAssignment)
+  .put(assignment.updateAssignment);
+
+
+app.route(prefix + '/assignments/:id')
+  .get(assignment.getAssignment)
+  .delete(assignment.deleteAssignment);
+
+  app.use(express.static(__dirname + "/frontdist/"));
+
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname + "/frontdist/index.html"));
+  });
+
+// On démarre le serveur
+app.listen(port, "0.0.0.0");
+console.log('Serveur démarré  sur http://localhost:' + port);
+
+module.exports = app;
+
+
